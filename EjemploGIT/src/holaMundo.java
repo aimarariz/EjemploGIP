9ad73d05a54d15436e4d249163ce90c1d67fff37
Aimar Ariznabarreta <151751919+aimarariz@users.noreply.github.com>
2024-09-23 08:12:06 +0200

    Cambio del mensage

--- EjemploGIT/src/holaMundo.java
+++ EjemploGIT/src/holaMundo.java
@@ -4,7 +4,7 @@ public class holaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Colega me voy de viaje");
+		System.out.println("ta peluche");
 
 	}
 
